4ec55f0a813205f7bc11d6108fc4bfb762271562
Shiva-99 <schiva1999@gmail.com>
2025-05-13 10:35:18 +0800

    Initial commit

---
+++ src/main/java/com/ewalletAccountService/EwalletAccountService.java
@@ -0,0 +1,13 @@
+package com.ewalletAccountService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EwalletAccountService {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EwalletAccountService.class, args);
+	}
+
+}
